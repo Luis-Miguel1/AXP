64e48073bdfe22a5bd61422542ace2cad31c467d
Luis-Miguel1 <luismtb2016@gmail.com>
2020-04-03 11:33:21 -0100

    web directory change gitignore

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -3,7 +3,7 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=associacao_xadrez2',
+            'dsn' => 'mysql:host=localhost;dbname=associacao_xadrez',
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8',
